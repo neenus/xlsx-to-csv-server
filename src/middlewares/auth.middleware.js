9a3a0903e80db6ca70ee6a5df239a5c120a53b23
Neenus Gabriel <hello@neenus.com>
2026-05-19 19:39:57 +0000

    chore(auth): replace local auth middleware with @nr/auth-middleware package

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,29 +1,3 @@
-const axios = require('axios');
-
-const requireAuth = async (req, res, next) => {
-  let token;
-  if (req.headers.authorization?.startsWith('Bearer ')) {
-    token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies?.token) {
-    token = req.cookies.token;
-  }
-
-  if (!token) {
-    return res.status(401).json({ success: false, error: 'Not authorized' });
-  }
-
-  try {
-    const authServiceUrl = process.env.AUTH_SERVICE_URL;
-    const appName = process.env.APP_NAME;
-    const response = await axios.get(
-      `${authServiceUrl}/api/v1/auth/validate?app=${appName}`,
-      { headers: { Authorization: `Bearer ${token}` } }
-    );
-    req.user = response.data.data.user;
-    next();
-  } catch (err) {
-    return res.status(401).json({ success: false, error: 'Not authorized' });
-  }
-};
+const { requireAuth } = require('@nr/auth-middleware');
 
 module.exports = { requireAuth };
